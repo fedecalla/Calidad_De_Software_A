809264ae88a8225422b21a374b36d3e97835649e
Fran Marino <franmarino06@gmail.com>
2026-09-17 10:52:43 -0300

    getters y setters

--- TPE2026/test/persistencia/AsociadoDTOTest.java
+++ TPE2026/test/persistencia/AsociadoDTOTest.java
@@ -1,93 +1,94 @@
 package persistencia;
 
-import static org.junit.Assert.*;
-import src.main.java.persistencia.AsociadoDTO
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AsociadoDTOTest {
-	
-	private AsociadoDTO entidad = new AsociadoDTO();
 
-	//Habria que probar los constructores ?
-	
-	@Test
-	public void testGetNombre() {
-		entidad.setNombre("Federico");
-		assertEquals("Federico", entidad.getNombre());
-	}
+    private final AsociadoDTO entidad = new AsociadoDTO();
 
-	@Test(expected = AssertionError.class)
-	public void testSetNombre_invalido() {
-		entidad.setNombre(null);
-	}
-	
-	@Test
-	public void testSetNombre_valido() {
-		entidad.setNombre("Pedro");
-		assertEquals("Pedro", entidad.getNombre());
-	}
+    @Test
+    public void testGetNombre() {
+        entidad.setNombre("Federico");
+        assertEquals("Federico", entidad.getNombre());
+    }
 
-	@Test
-	public void testGetApellido()
-	{
-		entidad.setApellido("Rodriguez");
-		assertEquals("Rodriguez", entidad.getApellido());
+    @Test(expected = AssertionError.class)
+    public void testSetNombreInvalido() {
+        entidad.setNombre(null);
+    }
 
-	}
+    @Test
+    public void testSetNombreValido() {
+        entidad.setNombre("Pedro");
+        assertEquals("Pedro", entidad.getNombre());
+    }
 
-	@Test
-	public void testSetApellido() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetApellido() {
+        entidad.setApellido("Rodriguez");
+        assertEquals("Rodriguez", entidad.getApellido());
+    }
 
-	@Test
-	public void testGetDni() {
-		fail("Not yet implemented");
-	}
+    @Test(expected = AssertionError.class)
+    public void testSetApellidoInvalido() {
+        entidad.setApellido(null);
+    }
 
-	@Test
-	public void testSetDni() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetDni() {
+        entidad.setDni("30123456");
+        assertEquals("30123456", entidad.getDni());
+    }
 
-	@Test
-	public void testGetCiudad() {
-		fail("Not yet implemented");
-	}
+    @Test(expected = AssertionError.class)
+    public void testSetDniInvalido() {
+        entidad.setDni(null);
+    }
 
-	@Test
-	public void testSetCiudad() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetCiudad() {
+        entidad.setCiudad("Córdoba");
+        assertEquals("Córdoba", entidad.getCiudad());
+    }
 
-	@Test
-	public void testGetCalle() {
-		fail("Not yet implemented");
-	}
+    @Test(expected = AssertionError.class)
+    public void testSetCiudadInvalido() {
+        entidad.setCiudad(null);
+    }
 
-	@Test
-	public void testSetCalle() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetCalle() {
+        entidad.setCalle("San Martín");
+        assertEquals("San Martín", entidad.getCalle());
+    }
 
-	@Test
-	public void testGetNumero() {
-		fail("Not yet implemented");
-	}
+    @Test(expected = AssertionError.class)
+    public void testSetCalleInvalido() {
+        entidad.setCalle(null);
+    }
 
-	@Test
-	public void testSetNumero() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetNumero() {
+        entidad.setNumero(1234);
+        assertEquals(1234, entidad.getNumero());
+    }
 
-	@Test
-	public void testGetTelefono() {
-		fail("Not yet implemented");
-	}
+    @Test(expected = AssertionError.class)
+    public void testSetNumeroInvalido() {
+        entidad.setNumero(0);
+    }
 
-	@Test
-	public void testSetTelefono() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetTelefono() {
+        entidad.setTelefono("3511234567");
+        assertEquals("3511234567", entidad.getTelefono());
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testSetTelefonoInvalido() {
+        entidad.setTelefono(null);
+    }
 
 }
