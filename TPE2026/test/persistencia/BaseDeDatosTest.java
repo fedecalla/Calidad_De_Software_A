db02f9ea6fa7fd89c4622d234fcc0d5a484b0f98
Fede Calla <fedecalla975@gmail.com>
2026-09-18 14:19:24 -0300

    BaseDeDatosTest y DataAccessObjectTest

---
+++ TPE2026/test/persistencia/BaseDeDatosTest.java
@@ -0,0 +1,49 @@
+package persistencia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaseDeDatosTest {
+
+	@Test
+	public void testGetInstancia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCerrarConexion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConexion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEjecutarConsulta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEjecutarActualizacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEjecutarInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEjecutarDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIniciarBD() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TPE2026/test/persistencia/DataAccessObjectTest.java
@@ -0,0 +1,59 @@
+package persistencia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataAccessObjectTest {
+
+	@Test
+	public void testDataAccessObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGuardar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCerrarConexion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCargarConLimite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCargar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCargarBD() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActualizar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrearDTOAsociado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrearDTOIntStringStringStringStringIntStringString() {
+		fail("Not yet implemented");
+	}
+
+}
